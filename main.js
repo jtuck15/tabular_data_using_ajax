840921b7b940f007822d6fe470f80031e89035c8
Jimmy Tuck <jim.tuck15@gmail.com>
2018-10-02 19:57:06 +0000

    Fixed bug at the top of the table

--- main.js
+++ main.js
@@ -62,6 +62,6 @@ function writeToDocument(url) {
 
         });
         
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }   
